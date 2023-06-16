af36ab6a76b21cd5c3b7a2f23f48482ecaac3521
codername-132 <nitin.rawat629@gmail.com>
2023-06-16 10:55:36 +0530

    add express middleware to parse request body and url

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const apiRoutes = require("./routes");
 const app = express();
 
 app.use("/api", apiRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server started listening on ${ServerConfig.PORT}`);
