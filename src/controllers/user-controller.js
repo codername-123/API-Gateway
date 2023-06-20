c874265f11d88f53faca53b9fe335e45763aecf0
codername-132 <nitin.rawat629@gmail.com>
2023-06-20 11:26:42 +0530

    Added controller layer for user to sign up

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,28 @@
+const { UserService } = require("../services");
+const { StatusCodes } = require("http-status-codes");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
+
+/**
+ * POST: /signup
+ * req.body: {email: 'abc@xyz.com', password: 'aksjsf'}
+ */
+async function createUser(req, res) {
+  try {
+    const user = await UserService.createUser({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    SuccessResponse.message = "Successfully created User";
+    SuccessResponse.data = user;
+
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statuscode).json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  createUser,
+};
