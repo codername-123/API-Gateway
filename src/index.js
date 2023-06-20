e1a503020eeebcaa8e77826f91f21fdd432f7a72
codername-132 <nitin.rawat629@gmail.com>
2023-06-20 12:14:48 +0530

    use express middlewares in correct order

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const apiRoutes = require("./routes");
 
 const app = express();
 
-app.use("/api", apiRoutes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server started listening on ${ServerConfig.PORT}`);
---
+++ src/routes/v1/user-routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { UserController } = require("../../controllers");
+const { UserMiddleware } = require("../../middlewares");
+
+const router = express.Router();
+
+/**
+ * route->  /api/v1/signup - POST
+ */
+router.post(
+  "/",
+  UserMiddleware.validateCreateRequestBody,
+  UserController.createUser
+);
