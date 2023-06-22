ed3cfe370c1237780b8024a53f6a108cf2837f49
codername-132 <nitin.rawat629@gmail.com>
2023-06-22 15:36:21 +0530

    Added rateLimiter logic to our application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
+const rateLimit = require("express-rate-limit");
 const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 3,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(limiter);
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
