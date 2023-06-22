d828daecb3eab35774c3c36fef78a422061cfe79
codername-132 <nitin.rawat629@gmail.com>
2023-06-22 16:45:31 +0530

    Added proxy middleware to handle routing to different services.

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -7,4 +7,6 @@ module.exports = {
   SALT: process.env.SALT,
   JWT_EXPIRES: process.env.JWT_EXPIRES,
   JWT_SECRET: process.env.JWT_SECRET,
+  FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
+  BOOKING_SERVICE: process.env.BOOKING_SERVICE,
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const rateLimit = require("express-rate-limit");
 const apiRoutes = require("./routes");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
@@ -14,6 +15,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
+
+app.use(
+  "/flightservice",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/flightservice": "/",
+    },
+  })
+);
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/bookingservice": "/",
+    },
+  })
+);
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
