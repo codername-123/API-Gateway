6c297443f2199be15a98a05613aef11139a9ec75
codername-132 <nitin.rawat629@gmail.com>
2023-06-28 11:48:16 +0530

    fix add onProxyReq: fixRequestBody so that the body parsed by the api gateway can be fixed

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const rateLimit = require("express-rate-limit");
 const apiRoutes = require("./routes");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {
+  createProxyMiddleware,
+  fixRequestBody,
+} = require("http-proxy-middleware");
 
 const app = express();
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
-  max: 3,
+  max: 20,
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
+
+// Todo: add auth middlewares to connect to flight service and booking service also make the admin checks
 app.use(
   "/flightservice",
   createProxyMiddleware({
     target: ServerConfig.FLIGHT_SERVICE,
     changeOrigin: true,
     pathRewrite: {
-      "^/flightservice": "/",
+      "^/flightservice": "",
     },
+    onProxyReq: fixRequestBody,
+    auth: true,
   })
 );
 app.use(
@@ -32,12 +39,11 @@ app.use(
     target: ServerConfig.BOOKING_SERVICE,
     changeOrigin: true,
     pathRewrite: {
-      "^/bookingservice": "/",
+      "^/bookingservice": "",
     },
+    onProxyReq: fixRequestBody,
   })
 );
-app.use("/api", apiRoutes);
-
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server started listening on ${ServerConfig.PORT}`);
 });
